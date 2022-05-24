333fbb284142cc5e96ed4f3fb835755f1cdd2674
Rafael Torquato <rafaeltorquat0@protonmail.com>
2022-05-23 21:20:38 -0300

    ✨ Added cli and fatjar.

--- src/main/java/com/torquato/springboot/vc/Main.java
+++ src/main/java/com/torquato/springboot/vc/Main.java
@@ -1,36 +1,14 @@
 package com.torquato.springboot.vc;
 
-import com.torquato.springboot.vc.application.ComparatorService;
-import com.torquato.springboot.vc.application.report.HtmlReportWriter;
-import com.torquato.springboot.vc.domain.model.dependency.ComparedDependenciesFactory;
-import com.torquato.springboot.vc.domain.model.dependency.DependenciesPairFactory;
-import com.torquato.springboot.vc.infrastructure.ExternalHtmlVersionService;
-
-import java.util.Set;
+import com.torquato.springboot.vc.delivery.cli.CompareCommand;
+import picocli.CommandLine;
 
 public class Main {
+
     //TODO create cli flow
     public static void main(final String[] args) {
-        if (!isValidArgs(args)) {
-            printManual();
-            return;
-        }
-        final ComparatorService comparatorService = new ComparatorService(
-                new ExternalHtmlVersionService(),
-                new ComparedDependenciesFactory(),
-                new DependenciesPairFactory(),
-                new HtmlReportWriter()
-        );
-        comparatorService.compare(Set.of("2.4.3", "2.6.7", "2.7.0"));
-    }
-
-    private static boolean isValidArgs(String[] args) {
-        //TODO cli params validation
-        return true;
-    }
-
-    private static void printManual() {
-        //TODO cli Manual
+        int exitCode = new CommandLine(new CompareCommand()).execute(args);
+        System.exit(exitCode);
     }
 
 }
---
+++ src/main/java/com/torquato/springboot/vc/delivery/cli/CompareCommand.java
@@ -0,0 +1,43 @@
+package com.torquato.springboot.vc.delivery.cli;
+
+import com.torquato.springboot.vc.application.ComparatorService;
+import com.torquato.springboot.vc.application.report.HtmlReportWriter;
+import com.torquato.springboot.vc.domain.model.dependency.ComparedDependenciesFactory;
+import com.torquato.springboot.vc.domain.model.dependency.DependenciesPairFactory;
+import com.torquato.springboot.vc.infrastructure.ExternalHtmlVersionService;
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
+
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+@CommandLine.Command(name = "compare", description = "Compare two or more SpringBoot versions.")
+public class CompareCommand implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"-v", "--versions"}, description = "SpringBoot versions. Ex: 2.4.3, 2.6.7, ...")
+    private String versions;
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            final ComparatorService comparatorService = new ComparatorService(
+                    new ExternalHtmlVersionService(),
+                    new ComparedDependenciesFactory(),
+                    new DependenciesPairFactory(),
+                    new HtmlReportWriter()
+            );
+            final Set<String> versionsSet = Stream.of(this.versions.split(","))
+                    .map(String::trim)
+                    .collect(Collectors.toSet());
+            comparatorService.compare(versionsSet);
+            return 0;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return 1;
+        }
+    }
+
+}
