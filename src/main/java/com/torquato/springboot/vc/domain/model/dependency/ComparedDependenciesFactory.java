a8ded3cf773aaa22b72300a484ab27fbdc65fcde
Rafael Torquato <rafaeltorquat0@protonmail.com>
2022-06-03 17:49:36 -0300

    ♻️ Minor refactoring

--- src/main/java/com/torquato/springboot/vc/domain/model/dependency/ComparedDependenciesFactory.java
+++ src/main/java/com/torquato/springboot/vc/domain/model/dependency/ComparedDependenciesFactory.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,32 +14,36 @@ import java.util.stream.Stream;
 @Slf4j
 public class ComparedDependenciesFactory {
 
+    private static final Comparator<ComparedDependency> DEPENDENCY_COMPARATOR =
+            Comparator.comparing((Function<ComparedDependency, String>) cd -> cd.dependency().groupId())
+                    .thenComparing(cd -> cd.dependency().artifactId());
+
     public ComparedDependencies create(final DependenciesPair pair) {
         log.info("Comparing {} with {}...", pair.left().version(), pair.right().version());
-        final Set<Dependency> allDependencies = Stream.of(pair.left(), pair.right())
-                .map(Dependencies::dependencies)
-                .flatMap(Collection::stream)
-                .map(VersionedDependency::dependency)
-                .collect(Collectors.toSet());
-        final Map<Dependency, List<VersionedDependency>> leftVersions = pair.left()
+        final Map<Dependency, List<VersionedDependency>> leftVersionsMap = pair.left()
                 .dependencies()
                 .stream()
                 .collect(Collectors.groupingBy(VersionedDependency::dependency));
-        final Map<Dependency, List<VersionedDependency>> rightVersions = pair.right()
+        final Map<Dependency, List<VersionedDependency>> rightVersionsMap = pair.right()
                 .dependencies()
                 .stream()
                 .collect(Collectors.groupingBy(VersionedDependency::dependency));
 
-        final Comparator<ComparedDependency> dependencyComparator =
-                Comparator.comparing((Function<ComparedDependency, String>) cd -> cd.dependency().groupId())
-                        .thenComparing(cd -> cd.dependency().artifactId());
-        final List<ComparedDependency> dependencies = allDependencies
+        final List<ComparedDependency> dependencies = Stream.of(pair.left(), pair.right())
+                .map(Dependencies::dependencies)
+                .flatMap(Collection::stream)
+                .map(VersionedDependency::dependency)
+                .collect(Collectors.toSet())
                 .stream()
                 .map(dependency -> {
-                    final List<VersionedDependency> leftDependencies = leftVersions.get(dependency);
-                    final List<VersionedDependency> rightDependencies = rightVersions.get(dependency);
-                    final String leftVersion = leftDependencies != null ? leftDependencies.get(0).version() : "none";
-                    final String rightVersion = rightDependencies != null ? rightDependencies.get(0).version() : "none";
+                    final String leftVersion = Optional.ofNullable(leftVersionsMap.get(dependency))
+                            .map(l -> l.get(0))
+                            .map(VersionedDependency::version)
+                            .orElse("none");
+                    final String rightVersion = Optional.ofNullable(rightVersionsMap.get(dependency))
+                            .map(l -> l.get(0))
+                            .map(VersionedDependency::version)
+                            .orElse("none");
                     final String diff = getDiff(leftVersion, rightVersion);
                     return new ComparedDependency(
                             dependency,
@@ -48,13 +52,13 @@ public class ComparedDependenciesFactory {
                             diff
                     );
                 })
-                .sorted(dependencyComparator)
+                .sorted(DEPENDENCY_COMPARATOR)
                 .collect(Collectors.toList());
         log.info("Comparing {} with {} DONE.", pair.left().version(), pair.right().version());
         return new ComparedDependencies(pair.left().version(), pair.right().version(), dependencies);
     }
 
-    private String getDiff(final String leftVersion, final String rightVersion) {
+    private static String getDiff(final String leftVersion, final String rightVersion) {
         if ("none".equals(leftVersion)) return "added";
         if ("none".equals(rightVersion)) return "removed";
 
